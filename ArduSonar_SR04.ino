3805f910f4dfe230b8358e08c03098f8bd36b2e5
vascojlp <vascojlp@gmail.com>
2016-03-10 20:33:16 +0000

    Added files via upload

---
+++ ArduSonar_SR04.ino
@@ -0,0 +1,19 @@
+#include <NewPing.h>
+
+ 
+#define TRIGGER_PIN 3
+#define ECHO_PIN 2
+#define MAX_DISTANCE 200
+ 
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+ 
+void setup() {
+   Serial.begin(9600);
+}
+ 
+void loop() {
+   delay(50);
+   unsigned int uS = sonar.ping_cm();
+   Serial.print(uS);
+   Serial.print("cm");
+}
